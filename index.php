1e742ff2a15997ad0144753e29d967e18d544132
Nicholas <nicholas.zx.99@gmail.com>
2019-04-29 00:14:21 +0200

    nav (no hide menu) and header

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/style.css">
+    <title>foodora</title>
+  </head>
+  <body>
+    <nav>
+      <div class="container">
+        <div class="left">
+          <a href="https://www.foodora.com/">
+            <img class="nav-logo" src="http://foodora.com/wp-content/uploads/2017/09/pandora_pink.svg">
+          </a>
+        </div>
+        <div class="right">
+
+        </div>
+      </div>
+    </nav>
+    <header>
+      <h1>GOOD FOOD FOR YOUR COUNTRY</h1>
+      <div class="country-box">
+        <div>
+          <small>CHOOSE YOUR COUNTRY:</small>
+        </div>
+        <div class="flags-container">
+          <a class="flag-img" href="https://www.foodora.at/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Austria">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/AT.png" alt="Austria">
+          </a>
+          <a class="flag-img" href="https://www.foodora.ca/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Canada">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/CA.png" alt="Canada">
+          </a>
+          <a class="flag-img" href="https://www.foodora.fi/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Finland">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/FL.png" alt="Finland">
+          </a>
+          <a class="flag-img" href="https://www.foodora.fr/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="France">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/FR.png" alt="France">
+          </a>
+          <a class="flag-img" href="https://www.foodora.de/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Germany">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/DE.png" alt="Germany">
+          </a>
+          <a class="flag-img" href="https://www.foodora.it/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Italy">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/IT.png" alt="Italy">
+          </a>
+          <a class="flag-img" href="https://www.foodora.nl/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Netherlands">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/NL.png" alt="Netherlands">
+          </a>
+          <a class="flag-img" href="https://www.foodora.no/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Norway">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/NO.png" alt="Norway">
+          </a>
+          <a class="flag-img" href="https://www.foodora.se/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Sweden">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/SE.png" alt="Sweden">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.pk/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Pakistan">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/PK.png" alt="Pakistan">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.sg/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Singapore">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/SG-1.png" alt="Singapore">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.my/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Malaysia">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/MY.png" alt="Malaysia">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.com.bd/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Bangladesh">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/bangladesh2.png" alt="Bangladesh">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.co.th/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Thailand">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/TH.png" alt="Thailand">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.hk/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Hong Kong">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/HK-1.png" alt="Hong Kong">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.com.tw/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Taiwan">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/TW.png" alt="Taiwan">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.ph/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Philippines">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/PH.png" alt="Philippines">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.bg/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Bulgaria">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/BG.png" alt="Bulgaria">
+          </a>
+          <a class="flag-img" href="https://www.foodpanda.ro/" data-toggle="tooltip" data-placement="top" title="" target="_blank" data-original-title="Romania">
+            <img src="https://www.foodora.com/wp-content/uploads/2015/07/RO.png" alt="Romania">
+          </a>
+        </div>
+      </div>
+    </header>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <footer></footer>
+  </body>
+</html>
